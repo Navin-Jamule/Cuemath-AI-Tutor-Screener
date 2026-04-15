7212e4a095c754953b5d546ca5a653d31babe508
Navin-Jamule <161733726+Navin-Jamule@users.noreply.github.com>
2026-04-15 21:09:12 +0530

    fixed the voicesignalExtractor

--- pages/api/transcribe.js
+++ pages/api/transcribe.js
@@ -8,7 +8,6 @@ import fs from "fs";
 import path from "path";
 import os from "os";
 import { extractVoiceSignals } from "../../Agents/VoiceSignalExtractor";
-
 export const config = {
   api: { bodyParser: false },
 };
